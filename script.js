11f7f6d168fa243aeeb0cf528dfe57f7b3224f21
MateusP21 <mateuspasso@outlook.com>
2021-12-10 19:20:18 -0300

    Requisito 9 & 10 - Finalizado

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const textoTarefa = document.getElementById('texto-tarefa');
 const listaTarefas = document.getElementById('lista-tarefas');
 const criarTarefa = document.getElementById('criar-tarefa');
+const apagaTudo = document.getElementById('apaga-tudo');
+const removerFinalizados = document.getElementById('remover-finalizados');
 const rgb128 = 'rgb(128, 128, 128)';
 let itemSelected = '';
 
@@ -13,16 +15,32 @@ function verifyItem(item) {
   itemSelected = item;
   itemSelected.style.backgroundColor = rgb128;
 }
+
+function completeItem(item) {
+  item.classList.toggle('completed');
+}
 function addTodo() {
   const li = document.createElement('li');
   li.addEventListener('click', (e) => verifyItem(e.target));
-
+  li.addEventListener('dblclick', (e) => completeItem(e.target));
   console.log(itemSelected);
   li.innerText = textoTarefa.value;
   listaTarefas.appendChild(li);
   textoTarefa.value = '';
 }
+function removeAll() {
+  listaTarefas.innerHTML = '';
+}
 
+function removeCompleted() {
+  for (let i = 0; i < listaTarefas.children.length; i += 1) {
+    if (listaTarefas.children[i].classList.contains('completed')) {
+      console.log(listaTarefas.children[i].remove());
+    }
+  }
+}
 window.onload = () => {
   criarTarefa.addEventListener('click', () => addTodo());
+  apagaTudo.addEventListener('click', () => removeAll());
+  removerFinalizados.addEventListener('click', () => removeCompleted());
 };
