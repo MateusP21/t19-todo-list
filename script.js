1834f9b6e2f7962d9ce7eea1b10101486e37e8b7
MateusP21 <mateuspasso@outlook.com>
2021-12-12 15:58:27 -0300

    Requisito 11 & 12 - Finalizado

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const listaTarefas = document.getElementById('lista-tarefas');
 const criarTarefa = document.getElementById('criar-tarefa');
 const apagaTudo = document.getElementById('apaga-tudo');
 const removerFinalizados = document.getElementById('remover-finalizados');
+const salvarTarefas = document.getElementById('salvar-tarefas');
+
 const rgb128 = 'rgb(128, 128, 128)';
 let itemSelected = '';
 
@@ -19,28 +21,55 @@ function verifyItem(item) {
 function completeItem(item) {
   item.classList.toggle('completed');
 }
-function addTodo() {
+
+const createTodo = (data) => {
   const li = document.createElement('li');
   li.addEventListener('click', (e) => verifyItem(e.target));
   li.addEventListener('dblclick', (e) => completeItem(e.target));
-  console.log(itemSelected);
-  li.innerText = textoTarefa.value;
-  listaTarefas.appendChild(li);
+  if (data.classList[0]) li.classList.add(data.classList[0]);
+  li.innerText = data.value;
+  return li;
+};
+function addTodo(item) {
+  if (!item && !textoTarefa.value) return;
+
+  if (item) {
+    listaTarefas.appendChild(createTodo(item));
+  } else {
+    listaTarefas.appendChild(createTodo(textoTarefa));
+  }
+
   textoTarefa.value = '';
 }
+
 function removeAll() {
   listaTarefas.innerHTML = '';
 }
 
 function removeCompleted() {
-  for (let i = 0; i < listaTarefas.children.length; i += 1) {
-    if (listaTarefas.children[i].classList.contains('completed')) {
-      console.log(listaTarefas.children[i].remove());
-    }
+  const itemsCompleted = document.getElementsByClassName('completed');
+
+  while (itemsCompleted.length) {
+    itemsCompleted[itemsCompleted.length - 1].remove();
   }
 }
+
+function saveTodo() {
+  const array = [...listaTarefas.children];
+  const listaTarefasArray = array.map((item) =>
+    ({ value: item.innerText, classList: item.classList }));
+  localStorage.setItem('data', JSON.stringify(listaTarefasArray));
+}
+
+function loadTodo() {
+  const data = JSON.parse(localStorage.getItem('data'));
+  if (data === null) return;
+  data.forEach((element) => addTodo(element));
+}
 window.onload = () => {
+  loadTodo();
   criarTarefa.addEventListener('click', () => addTodo());
   apagaTudo.addEventListener('click', () => removeAll());
   removerFinalizados.addEventListener('click', () => removeCompleted());
+  salvarTarefas.addEventListener('click', () => saveTodo());
 };
